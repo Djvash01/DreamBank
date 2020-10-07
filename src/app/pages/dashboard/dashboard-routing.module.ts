f7e94dba5520dca2c394c137ff3619d2c06dfa36
David Yepes <djvash01@gmail.com>
2020-10-06 19:54:44 -0500

    add guards to paths

--- src/app/pages/dashboard/dashboard-routing.module.ts
+++ src/app/pages/dashboard/dashboard-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from 'src/app/guards/auth.guard';
 
 import { DashboardComponent } from './dashboard.component';
 
-const routes: Routes = [{ path: '', component: DashboardComponent }];
+const routes: Routes = [
+  { 
+    path: '',
+    canActivate: [AuthGuard], 
+    component: DashboardComponent 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
