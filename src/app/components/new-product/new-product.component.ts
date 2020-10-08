8848bff3946dc7fb864d40d2fb0c41bb07c59d40
David Yepes <djvash01@gmail.com>
2020-10-08 17:46:07 -0500

    Create view html to request new product

--- src/app/components/new-product/new-product.component.ts
+++ src/app/components/new-product/new-product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-new-product',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-product.component.scss']
 })
 export class NewProductComponent implements OnInit {
+  
+  productsList = ['leasing','foreing currency exchange','Bank guarantee','remittance of founds','credit cards', 'debit cards'];
 
-  constructor() { }
+  form:FormGroup;
+
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      product: ['', Validators.required],
+      cellphone: ['', Validators.required],
+      monthly: ['', Validators.required]
+    })
+  }
+
+  submit(){
+
   }
 
 }
--- src/app/shared/angular-material/angular-material.module.ts
+++ src/app/shared/angular-material/angular-material.module.ts
@@ -14,6 +14,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatBadgeModule} from '@angular/material/badge';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatTableModule} from '@angular/material/table';
+import {MatSelectModule} from '@angular/material/select';
 
 
 
@@ -35,7 +36,8 @@ import {MatTableModule} from '@angular/material/table';
     MatToolbarModule,
     MatBadgeModule,
     MatMenuModule,
-    MatTableModule
+    MatTableModule,
+    MatSelectModule
   ],
   exports: [
     MatButtonModule, 
@@ -51,7 +53,8 @@ import {MatTableModule} from '@angular/material/table';
     MatToolbarModule,
     MatBadgeModule,
     MatMenuModule,
-    MatTableModule
+    MatTableModule,
+    MatSelectModule
   ],
 })
 export class AngularMaterialModule {}
