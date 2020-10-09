7b6de28a0b5877e37f54269a468af87637c9781d
David Yepes <djvash01@gmail.com>
2020-10-08 22:28:45 -0500

    Add logo moon svg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DreamBank';
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer){
+    iconRegistry.addSvgIcon(
+      'moon',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/moon.svg'));
+  }
 }
